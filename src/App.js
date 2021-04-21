5cff4c25721fc9b513dbd1df4816de277f5fcf98
vamsikrishnadev <vamsikrishnadev@gmail.com>
2021-04-21 12:51:49 +0530

    MobX

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Children } from 'react'
+import {useLocalStore,useObserver} from 'mobx-react'
+
+
+
+const GamesContext=React.createContext()
+const GamesProvider=({children})=>{
+  const store=useLocalStore(()=>(
+    {
+      games:["Cricket","Hockey"],
+      addGame:(game)=>{
+        store.games.push(game)
+      },
+      get gamesCount(){
+        return this.games.length
+      }
+     } 
+  )
+  )
+  return(
+    <GamesContext.Provider value={store}>
+      {children}
+    </GamesContext.Provider>
+  )
+}
+
+const GamesList=()=>{
+  const store=React.useContext(GamesContext)
+  return useObserver(()=>{
+     return   <div>Games Count {store.gamesCount}
+       <ul>
+            { store.games.map(game=>(
+                <li key={game}>{game}</li>
+            ))}
+        </ul>
+        </div>
+    })
+  }
+
+const AddNewGame=()=>{
+  const [txtGame,setTxtGame]=React.useState('')
+  const store=React.useContext(GamesContext)
+  const addGame=(e)=>{
+    e.preventDefault()
+    store.addGame(txtGame)
+    setTxtGame('')
+  }
+  return (
+    <form onSubmit={(e)=>e.preventDefault()}>
+      <input type="text" 
+        name="txtGame" 
+        value={txtGame} 
+        onChange={(e)=>{setTxtGame(e.target.value)}}
+      />
+      <button onClick={addGame}>Submit</button>
+    </form>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GamesProvider>
+      <AddNewGame/>
+      <GamesList/>
+    </GamesProvider>
   );
 }
 
